15d3e9fd93db1b400e129cc4fd86dedc485ea2e3
Andrey <86359412+Santas7@users.noreply.github.com>
2024-05-07 12:35:52 -0700

    обновление компоненты капчи

--- src/components/Captcha/captcha.component.ts
+++ src/components/Captcha/captcha.component.ts
@@ -1,13 +1,40 @@
-import {Component} from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 import {RouterLink} from "@angular/router";
+import {FormsModule} from "@angular/forms";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'Captcha',
   templateUrl: './captcha.component.html',
+  styleUrls: ['./captcha.component.css'],
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    FormsModule,
+    NgIf
   ]
 })
-export class CaptchaComponent {}
+export class CaptchaComponent implements OnInit {
+  generatedCaptcha?: string;
+  userInput?: string;
+  isCaptchaValid?: boolean;
 
+  constructor() { }
+
+  ngOnInit(): void {
+    this.generateCaptcha();
+  }
+
+  generateCaptcha(): void {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let captcha = '';
+    for (let i = 0; i < 6; i++) {
+      captcha += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    this.generatedCaptcha = captcha;
+  }
+
+  checkCaptcha(): void {
+    this.isCaptchaValid = this.userInput === this.generatedCaptcha;
+  }
+}
