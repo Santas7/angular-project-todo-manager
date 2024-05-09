6d13d5bf6f04661b90c4022fa899d5bf3680cb47
Santas7 <andreypanyshkin03@gmail.com>
2024-05-09 01:59:47 -0700

    Изменил компоненту App. Добавил комментарий app component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // addTodo(newTodo: string): void {
-  //   this.todos.push(newTodo);
-  // }
-  // deleteTodo(index: number): void {
-  //   this.todos.splice(index, 1);
-  // }
+  // app component
 }
