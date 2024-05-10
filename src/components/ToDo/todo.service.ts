698bc87889318121c9804980d7c298c4e0387023
Santas7 <andreypanyshkin03@gmail.com>
2024-05-10 05:19:33 -0700

    Добавил fix для todo + store

--- src/components/ToDo/todo.service.ts
+++ src/components/ToDo/todo.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { AuthService } from '../../bll/store';
 
 @Injectable({
     providedIn: 'root'
@@ -8,9 +9,8 @@ export class TodoService {
     private todoListSubject: BehaviorSubject<string[]> = new BehaviorSubject<string[]>([]);
     public todoList$: Observable<string[]> = this.todoListSubject.asObservable();
 
-    constructor() {
-        // Предположим, что данные изначально приходят из bll store.ts и передаются сюда
-        const initialData: string[] = ["ToDo 1", "ToDo 2", "ToDo 3"];
+    constructor(private authService: AuthService) {
+        const initialData = this.authService.getTodoList();
         this.setTodoList(initialData);
     }
 
@@ -22,12 +22,16 @@ export class TodoService {
         const currentList = this.todoListSubject.value;
         const updatedList = [...currentList, todo];
         this.todoListSubject.next(updatedList);
+        this.authService.addNote(todo); // Добавляем заметку в todoList в localStorage
     }
 
     public deleteTodo(todo: string | undefined): void {
         const currentList = this.todoListSubject.value;
         const updatedList = currentList.filter(item => item !== todo);
         this.todoListSubject.next(updatedList);
+        if (todo != null) {
+            this.authService.deleteNote(currentList.indexOf(todo));
+        } // Удаляем заметку из todoList в localStorage
     }
 
     public getTodoList(): Observable<string[]> {
